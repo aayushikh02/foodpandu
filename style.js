11aba55aed2dc1edde6294858ae70c5ddf83ea74
aayushikh02 <aayushi.kh02@gmail.com>
2020-10-12 19:30:11 +0530

    Alert added

---
+++ style.js
@@ -0,0 +1,13 @@
+card_data = [
+  {
+    name: "Water Bottle",
+    quantity: "500mL",
+    category: "All Day Breakfast",
+    stars: 5,
+    price: 20,
+    specification: ["white", "long", "plastic"],
+    description: "Carbonated Jeera Masala Ingredients are",
+  },
+];
+
+alert("This alert .....box was called with the onload event");
